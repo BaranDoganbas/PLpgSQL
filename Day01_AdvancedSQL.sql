915bdddc5bfc7173ac8cacee089a933207e72c94
Baran <barandoganbas@gmail.com>
2023-03-07 00:49:01 +0300

    07.03

--- Day01_AdvancedSQL.sql
+++ Day01_AdvancedSQL.sql
@@ -38,7 +38,128 @@ BEGIN
 END $$ ;
 
 
+--**************************************************************
+--***********************VARIABLES - CONSTANT*******************
+--**************************************************************
 
+DO $$
+DECLARE
+	counter		integer :=1;
+	first_name	varchar(50)	 := 'John';
+	last_name	varchar(50)	 := 'Doe';
+	payment		numeric(4,2) :=  20.5;
+BEGIN
 
+	RAISE NOTICE '% % % has been paid % USD', 
+				 counter,
+				 first_name,
+				 last_name,
+				 payment;		 
+END $$;
+
+-- Task 1 : değişkenler oluşturarak ekrana " Ahmet ve Mehmet beyler 120 tl ye bilet aldılar. "" cümlesini ekrana basınız
+
+DO $$
+DECLARE
+	first_person  varchar(30):= 'Ahmet';
+	second_person varchar(30):= 'Mehmet';
+	payment		  numeric (3) := 120;
+BEGIN
+
+	RAISE NOTICE '% ve % beyler % TL ye bilet aldilar',
+					first_person,
+					second_person,
+					payment;
+END $$
+
+-- ********************* BEKLETME KOMUDU *********************
+
+DO $$
+DECLARE
+	created_at time := now();
+BEGIN
+	RAISE NOTICE '%', created_at;
+	PERFORM pg_sleep(10);		-- 10 saniye bekleniyor.
+	RAISE NOTICE '%', created_at;	-- Ciktida ayni deger gorunecek
+END $$
+
+-- ********************* TABLODAN DATA TIPINI KOPYALAMA *********************
+
+	/*
+		-> variable_name  table_name.column_name%type;
+		->( Tablodaki datanın aynı data türünde variable oluşturmaya yarıyor)
+	*/
+	
+DO $$
+DECLARE
+	film_title film.title%TYPE;  -- varchar;
+BEGIN
+	-- 1 id'li film'in ismini getirelim
+	SELECT title
+	FROM film
+	INTO film_title  -- film_title = 'Kuzularin Sessizligi'
+	WHERE id=1;
+	
+	RAISE NOTICE 'Film title id 1 : %', film_title;
+END $$;	
+	
+--************************* IC ICE BLOK YAPILARI *************************
+
+DO $$
+<<outher_block>>
+DECLARE
+	counter integer :=0;
+BEGIN
+	counter := counter + 1;
+	RAISE NOTICE 'The current value of counter is %', counter;
+	
+	DECLARE
+		counter integer :=0;
+		
+	BEGIN
+		counter := counter + 10;
+		RAISE NOTICE 'Counter in the subBlock is %', counter;
+		RAISE NOTICE 'Counter in the OutherBlock is %', outher_block.counter;
+	END;
+	
+	RAISE NOTICE 'Counter in the outherBlock is %', counter;
+
+END outher_block $$;
+
+-- ******************* Row Type *******************
+
+DO $$
+DECLARE
+	selected_actor	actor%ROWTYPE;
+BEGIN
+	SELECT *	--	id, isim, soyisim
+	FROM actor
+	INTO selected_actor	 -- id,isim ,soyisim
+	WHERE id=1;
+	
+	RAISE NOTICE 'The actor name is % %',
+					selected_actor.isim,
+					selected_actor.soyisim;
+END $$;
+
+-- ****************** Record Type ******************
+
+	/*
+	 -> Row Type gibi çalışır ama record un tamamı değilde belli başlıkları almak
+		istersek kullanılabilir
+	*/
+	
+DO $$
+DECLARE
+	rec record;	--	record data turunde rec isminde variable olusturuldu
+BEGIN
+	SELECT id,title,type
+	INTO rec
+	FROM film
+	WHERE id = 1;
+
+	RAISE NOTICE '% % %', rec.id, rec.title, rec.type;
+	
+END $$;
 
 
